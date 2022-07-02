5d74b60248a65b5a8342974f20ac2148f0d88cd4
zandanipratish <pratishzandani@gmail.com>
2022-07-02 17:14:43 +0530

    New Deployment with docker image

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app=Flask(__name__)
 
+
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting ML peoject"
+    return "This is my first Project Deployment"
+
 
 if __name__=="__main__":
-   app.run(debug=True) 
+    app.run(debug=True)
